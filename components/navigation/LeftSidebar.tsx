8d21d6243a430cfbd3a87ac6f09e25c5865361e3
MohamedAbdelhay00 <mohamed.a.abdelhay55@gmail.com>
2024-12-17 12:30:26 +0300

    Implement the left and right sidebar

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -6,10 +6,11 @@ import { Button } from "../ui/button";
 import Image from "next/image";
 
 const LeftSidebar = () => {
+  const isMobile = false;
   return (
     <section className="no-scrollbar overflow-hidden custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-3">
-        <NavLinks />
+        <NavLinks isMobile={isMobile} />
       </div>
 
       <div className="flex flex-col gap-3">
