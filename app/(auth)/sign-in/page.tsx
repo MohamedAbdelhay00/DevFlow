c2503323bdde3be95426b55d1a4cbac646be96dd
MohamedAbdelhay00 <mohamed.a.abdelhay55@gmail.com>
2025-01-16 15:57:30 +0200

    Implement Password/Email SignIn

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import React from "react";
+
 import AuthForm from "@/components/forms/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
 import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
   return (
     <div>
       <AuthForm
+        formType="SIGN_IN"
         schema={SignInSchema}
         defaultValues={{ email: "", password: "" }}
-        formType="SIGN_IN"
-        onSubmit={(data) => Promise.resolve({ success: true, data })}
+        onSubmit={signInWithCredentials}
       />
     </div>
   );
